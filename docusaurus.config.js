5773d319653581299a9bbeec6f6abf70580f1cdd
Evan <123332223+EvanDevvv@users.noreply.github.com>
2023-07-20 19:34:04 -0400

    e

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EvanDevvv', // Usually your GitHub org/user name.
-  projectName: 'GTSAssets', // Usually your repo name.
+  projectName: 'evandevvv.github.io', // Usually your repo name.
 
   deploymentBranch: "gh-pages",
 
